f1bd118ac7f6211573ba32240ab343fdf00ad1d1
ianer-glitch <ianrepker@gmail.com>
2024-03-14 20:55:22 -0300

    configuring directories

--- exampleApp/vite.config.ts
+++ exampleApp/vite.config.ts
@@ -8,6 +8,21 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  
+  server:{
+		// host true ensure that we can acess the app from outside container
+    host:true,
+    port:8000,
+    // usePolling will ensure that node's auto reload gonna work.
+    watch:{
+      usePolling:true
+    }
+  },
+
+  preview:{
+    port:8000
+  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
